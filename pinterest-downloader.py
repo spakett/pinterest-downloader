f7e8a5e9933aca5566160164633d20b729d7573e
Ville Ylönen <villeylonen8@gmail.com>
2026-02-27 00:02:35 +0200

    feat: add functionality for fetching user's board URLs

--- pinterest-downloader.py
+++ pinterest-downloader.py
@@ -7,12 +7,21 @@ def log_user_in(driver):
     input('Please log in and press Enter to continue')
     user_url = driver.find_element(By.CSS_SELECTOR, 'a[aria-label="Your profile"]').get_attribute('href')
     driver.get(user_url)
-    sleep(5)
+    sleep(5) # Wait for the page to load
 
+def get_boards(driver):
+    board_html_elements = driver.find_elements(By.CSS_SELECTOR, '[data-test-id="pwt-grid-item"]')
+    board_links = []
+    for board in board_html_elements:
+        board_links.append(board.find_element(By.TAG_NAME, 'a').get_attribute('href'))
+    return board_links
+
+    
 def main():
     print("Opening web driver. Please wait.")
     driver = webdriver.Firefox()
     log_user_in(driver)
+    board_urls = get_boards(driver)
 
     driver.close()
 
