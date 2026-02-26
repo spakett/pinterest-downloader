880c90c48bd833cd3d371ca87f1df05a6ab54176
Ville Ylönen <villeylonen8@gmail.com>
2026-02-26 17:53:27 +0200

    feat: add functionality for logging the user in

--- pinterest-downloader.py
+++ pinterest-downloader.py
@@ -1,10 +1,20 @@
-def get_user_boards(profile_url):
-    pass
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+def log_user_in(driver):
+    driver.get('https://pinterest.com/')
+    input('Please log in and press Enter to continue')
+    user_url = driver.find_element(By.CSS_SELECTOR, 'a[aria-label="Your profile"]').get_attribute('href')
+    driver.get(user_url)
+    sleep(5)
 
 def main():
-    profile_url = input("Input the profile's URL: ")
-    print(f"Opening '{profile_url}'")
-    get_user_boards(profile_url)
+    print("Opening web driver. Please wait.")
+    driver = webdriver.Firefox()
+    log_user_in(driver)
+
+    driver.close()
 
 if __name__ == '__main__':
     main()
