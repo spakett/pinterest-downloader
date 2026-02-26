f9542c397dd0c76a52ffb9f7c23fa961b30b164c
Ville Ylönen <villeylonen8@gmail.com>
2026-02-26 14:15:50 +0200

    feat: add functionality for reading user input

---
+++ pinterest-downloader.py
@@ -0,0 +1,10 @@
+def get_user_boards(profile_url):
+    pass
+
+def main():
+    profile_url = input("Input the profile's URL: ")
+    print(f"Opening '{profile_url}'")
+    get_user_boards(profile_url)
+
+if __name__ == '__main__':
+    main()
