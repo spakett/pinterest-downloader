e75601e6405a1c7268e3bb4c0cdc2c015f83c08d
Ville Ylönen <villeylonen8@gmail.com>
2026-02-27 21:11:47 +0200

    feat: add functionality for automatically scrolling a board

--- pinterest-downloader.py
+++ pinterest-downloader.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from time import sleep
+
 
 def log_user_in(driver):
     driver.get('https://pinterest.com/')
@@ -9,53 +11,75 @@ def log_user_in(driver):
     driver.get(user_url)
     sleep(3) # Wait for the page to load
 
-def get_boards(driver):
+
+def get_board_urls(driver):
     board_html_elements = driver.find_elements(By.CSS_SELECTOR, '[data-test-id="pwt-grid-item"]')
-    board_links = []
+    board_urls = []
+
     for board in board_html_elements:
-        board_links.append(board.find_element(By.TAG_NAME, 'a').get_attribute('href'))
-    return board_links
+        board_urls.append(board.find_element(By.TAG_NAME, 'a').get_attribute('href'))
+
+    return board_urls
+
 
 def get_pins_from_board(driver):
-    
     board_name = driver.find_element(By.CSS_SELECTOR, '[id="board-name"]').text
     pin_count = int(driver.find_element(By.CSS_SELECTOR, '[data-test-id="pin-count"]').text.split()[0])
-    removed_pin_count = len(driver.find_elements(By.CSS_SELECTOR, '[data-test-id="unavailable-pin"]'))
     
     if not pin_count:
         print(f'Board "{board_name}" contains no pins.')
-        return {}
+        return []
+    
+    # TODO: Sections break the current logic. Search for sections and process them before processing the main board. 
     
     print(f'Now processing board "{board_name}" with {pin_count} pins.')
 
     pins_metadata = {} # Structure {pin_id: (href, is_removed)}
     highest_encountered_idx = -1
 
-    while highest_encountered_idx < pin_count:
+    while highest_encountered_idx < (pin_count - 1):
         pin_html_elements = driver.find_elements(By.CSS_SELECTOR, '[data-test-id="pin"]')
         for pin in pin_html_elements:
             pin_idx = int(pin.get_attribute('data-test-pin-slot-index'))
             highest_encountered_idx = pin_idx if pin_idx > highest_encountered_idx else highest_encountered_idx
             if pin_idx not in pins_metadata.keys():
                 pin_href = pin.find_element(By.TAG_NAME, 'a').get_attribute('href')
+
+                # Information about the pin being unavailable is located in a child element
                 is_pin_unavailable = False
-                if len(pin.find_element(By.CSS_SELECTOR, '[data-test-id="unavailable-pin"]')):
+                try:
+                    pin.find_element(By.CSS_SELECTOR, '[data-test-id="unavailable-pin"]')
                     is_pin_unavailable = True
-                pins_metadata[pin_idx] = [pin_href, is_pin_unavailable]
+                except NoSuchElementException:
+                    pass
+
+                pins_metadata[pin_idx] = [pin_href, board_name, is_pin_unavailable]
+        
+        # Scroll down and wait for new pins to load
+        driver.execute_script('window.scrollBy(0, 250);')
+        sleep(1)
+
+        print(f'{len(pins_metadata.keys())} of {pin_count} pins have been processed.')
+    print(f'All pins from board {board_name} processed - moving onto the next board.')
+
+    # The pin index is no longer needed
+    return pins_metadata.values()
+
 
 def main():
     print("Opening web driver. Please wait.")
     driver = webdriver.Firefox()
     log_user_in(driver)
-    board_urls = get_boards(driver)
+    board_urls = get_board_urls(driver)
 
+    pins_metadata = []
     for url in board_urls:
         driver.get(url)
         sleep(3) # Wait for the page to load
-        get_pins_from_board(driver)
-        break
+        pins_metadata += get_pins_from_board(driver)
 
     driver.close()
+
 
 if __name__ == '__main__':
     main()
